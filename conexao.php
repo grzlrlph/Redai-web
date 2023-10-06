661bc2c044544e5778166a062663428a50b1d6f7
Graziela Ralph <113774678+grzlrlph@users.noreply.github.com>
2023-10-06 16:12:58 -0400

    back do cadastro (alguem coloca css nessa pag, ela ta mto feia)

---
+++ conexao.php
@@ -0,0 +1,19 @@
+<?php
+    class db{
+        private $host = 'localhost';
+
+        private $usuario = 'root';
+
+        private $senha = '';
+
+        private $database = 'redaidb';
+
+        public function conecta_mysql(){
+            $con = mysqli_connect($this->host, $this->usuario, 
+            $this->senha ,$this->database);
+    
+            return $con;
+    
+        }
+        
+    }
