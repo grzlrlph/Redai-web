f7d6daa6d03a57f6f5bbd3097541f9193cc7998e
Graziela Ralph <113774678+grzlrlph@users.noreply.github.com>
2023-10-10 11:17:35 -0400

    solucao??????
    nao testei e afins

--- back/processa_login.php
+++ back/processa_login.php
@@ -1,13 +1,25 @@
 <?php
-    require_once('conexao.php');
+ include ('conexao.php');
 
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+ if(empty($_POST['email'] || empty($_POST['senha']))){
+    //header ('Location: login.html');
 
-    $bancoDados = new db();
+ }
+
+ $bancoDados = new db();
 
     $link = $bancoDados-> conecta_mysql();
 
+ $email = mysqli_real_escape_string ($link, $_POST['email']);
+ $senha = mysqli_real_escape_string ($link, $_POST['senha']);
+
+$query = "SELECT * from usuario where email= '$email' and senha = '$senha'";
+echo query; exit; 
+
+$result = mysqli_query($link, $query);
+
+$row = mysqli_num_rows($result);
+echo $result;
 
 
 
